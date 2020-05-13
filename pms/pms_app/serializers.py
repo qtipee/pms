b23265c1c97cd17f94883cf6ab8371eef2e81f7e
qtipee <quent.vallat@gmail.com>
2020-05-13 14:01:12 +0200

    DateTime field format

--- pms/pms_app/serializers.py
+++ pms/pms_app/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+from datetime import datetime
 from .models import *
 
 class ImageSerializer(serializers.ModelSerializer):
+    # DateTime field format
+    datetime = serializers.DateTimeField(format='%d.%m.%Y %H:%M')
     
     class Meta:
         model = Image
-        fields = '__all__'
+        fields = ('id', 'datetime', 'count')
